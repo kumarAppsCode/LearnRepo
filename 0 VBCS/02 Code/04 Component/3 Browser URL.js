eb6dc5c9cae8d55478b8b6e7d5b89c1c557b3446
Dinesh <pydinesh3@gmail.com>
2023-08-09 00:48:04 +0530

    Create 3 Browser URL.js

---
+++ 0 VBCS/02 Code/04 Component/3 Browser URL.js
@@ -0,0 +1,10 @@
+        console.log("window.location.href===>"+window.location.href);
+        console.log("window.location.hostname===>"+window.location.hostname);
+        console.log("window.location.pathname===>"+window.location.pathname);
+        console.log("window.location.protocol===>"+window.location.protocol);
+        
+		
+		if(window.location.href === "https://*******.oraclecloud.com"){
+	  
+	  
+		}
