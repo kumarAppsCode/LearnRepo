ad0409fd9179c27e3a6479b6537f220dfe8f260f
Dinesh <pydinesh3@gmail.com>
2023-11-26 12:13:46 +0530

    Update 0 Blank Page.js

--- 0 VBCS/02 Code/12 JavaScript/1 Page/0 Blank Page.js
+++ 0 VBCS/02 Code/12 JavaScript/1 Page/0 Blank Page.js
@@ -1,1 +1,23 @@
-hi
+define([
+  'vb/action/actionChain',
+  'vb/action/actions',
+  'vb/action/actionUtils',
+], (
+  ActionChain,
+  Actions,
+  ActionUtils
+) => {
+  'use strict';
+
+  class temp extends ActionChain {
+
+    /**
+     * @param {Object} context
+     */
+    async run(context) {
+      const { $page, $flow, $application } = context;
+    }
+  }
+
+  return temp;
+});
