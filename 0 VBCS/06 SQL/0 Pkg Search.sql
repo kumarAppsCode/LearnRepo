a4a3b4de5c85cd50f7f586ab307024857ca1ac73
Dinesh <pydinesh3@gmail.com>
2024-01-02 12:30:43 +0530

    Update 0 Pkg Search.sql

--- 0 VBCS/06 SQL/0 Pkg Search.sql
+++ 0 VBCS/06 SQL/0 Pkg Search.sql
@@ -1,46 +1,52 @@
-create or replace PROCEDURE xx_getrecord(p_bu_id        IN     NUMBER
-                                        ,p_contract_num IN     VARCHAR2
-                                        ,p_contract_status IN  VARCHAR2
-                                        ,p_vendor_id    IN     NUMBER
-                                        ,p_approval_status_id IN NUMBER
-                                        ,p_from_contract_date IN DATE
-                                        ,p_to_contract_date IN DATE
-                                        ,p_output          OUT SYS_REFCURSOR
-                                        ,p_sql             OUT VARCHAR2)
+create or replace PROCEDURE RH_LOGGING_SEARCH(
+                                        p_integration_name      in varchar2,
+                                        p_from_run_date         in varchar2,
+                                        p_to_run_date           in varchar2,
+                                        p_user_name             in varchar2,
+                                        p_log_type              in varchar2,
+                                        p_channel_type          in varchar2,       
+                                        p_output          		out sys_refcursor,
+                                        p_sql             		out varchar2
+                                        )
 IS
    l_sql          VARCHAR2(4000);
    l_where        VARCHAR2(4000) := 'WHERE 1=1';
 BEGIN
-   IF p_bu_id IS NOT NULL THEN
-      l_where     := l_where || ' AND BU_ID=' || p_bu_id;
+
+--   IF p_integration_name IS NOT NULL THEN
+--      l_where     := l_where || ' AND UPPER(integration_name) LIKE ''%' || UPPER(p_integration_name) || '%''';
+--   END IF;
+--
+   IF p_from_run_date IS NOT NULL THEN
+      l_where     := l_where || ' AND trunc(RUN_DATE)>=' ||'to_date('''||p_from_run_date || ''',''YYYY-MM-DD'')' ;  
    END IF;
 
-   IF p_contract_num IS NOT NULL THEN
-      l_where     := l_where || ' AND UPPER(CONTRACT_NUM) LIKE ''%' || UPPER(p_contract_num) || '%''';
+   IF p_to_run_date IS NOT NULL THEN
+         l_where     := l_where || ' AND trunc(RUN_DATE)<=' ||'to_date('''||p_to_run_date || ''',''YYYY-MM-DD'')' ;  
+        
    END IF;
 
-   IF p_contract_status IS NOT NULL THEN
-      l_where     := l_where || ' AND UPPER(CONTRACT_STATUS) LIKE ''%' || UPPER(p_contract_status) || '%''';
-   END IF;
+--   IF p_from_run_date IS NOT NULL THEN
+--      l_where     := l_where || ' AND UPPER(CONTRACT_NUM) LIKE ''%' || UPPER(p_contract_num) || '%''';
+--   END IF;
 
-   IF p_vendor_id IS NOT NULL THEN
-      l_where     := l_where || ' AND VENDOR_ID=' || p_vendor_id;
-   END IF;
+--   IF p_user_name IS NOT NULL THEN
+--      l_where     := l_where || ' AND UPPER(user_name) LIKE ''%' || UPPER(p_user_name) || '%''';
+--   END IF;
+--
+--   IF p_log_type IS NOT NULL THEN
+--      l_where     := l_where || ' AND UPPER(log_type) LIKE ''%' || UPPER(p_log_type) || '%''';
+--   END IF;
+--
+--   IF p_channel_type IS NOT NULL THEN
+--      l_where     := l_where || ' AND UPPER(channel_type) LIKE ''%' || UPPER(p_channel_type) || '%''';
+--   END IF;
 
-   IF p_approval_status_id IS NOT NULL THEN
-      l_where     := l_where || ' AND APPROVAL_STATUS_ID=' || p_approval_status_id;
-   END IF;
 
-   IF p_from_contract_date IS NOT NULL THEN
-      l_where     := l_where || ' AND CONTRACT_DATE>=''' || p_from_contract_date || '''';
-   END IF;
 
-   IF p_to_contract_date IS NOT NULL THEN
-      l_where     := l_where || ' AND CONTRACT_DATE<=''' || p_to_contract_date || '''';
-   END IF;
-
-   l_sql       := 'SELECT * FROM SC_CONTRACT_HEADERS_V ' || l_where;
+   l_sql       := 'SELECT * FROM Rh_common_logging ' || l_where;
    p_sql       := l_sql;
+----
 
    OPEN p_output FOR l_sql;
-END xx_getrecord;
+END RH_LOGGING_SEARCH;
