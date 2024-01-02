8c6a20786e1f82fff5bd3201a0c9abbf32e4c6bf
Dinesh <pydinesh3@gmail.com>
2024-01-02 12:32:43 +0530

    Update 0 Pkg Search.sql

--- 0 VBCS/06 SQL/0 Pkg Search.sql
+++ 0 VBCS/06 SQL/0 Pkg Search.sql
@@ -13,10 +13,10 @@ IS
    l_where        VARCHAR2(4000) := 'WHERE 1=1';
 BEGIN
 
---   IF p_integration_name IS NOT NULL THEN
---      l_where     := l_where || ' AND UPPER(integration_name) LIKE ''%' || UPPER(p_integration_name) || '%''';
---   END IF;
---
+   IF p_integration_name IS NOT NULL THEN
+      l_where     := l_where || ' AND UPPER(integration_name) LIKE ''%' || UPPER(p_integration_name) || '%''';
+   END IF;
+
    IF p_from_run_date IS NOT NULL THEN
       l_where     := l_where || ' AND trunc(RUN_DATE)>=' ||'to_date('''||p_from_run_date || ''',''YYYY-MM-DD'')' ;  
    END IF;
@@ -26,21 +26,18 @@ BEGIN
         
    END IF;
 
---   IF p_from_run_date IS NOT NULL THEN
---      l_where     := l_where || ' AND UPPER(CONTRACT_NUM) LIKE ''%' || UPPER(p_contract_num) || '%''';
---   END IF;
 
---   IF p_user_name IS NOT NULL THEN
---      l_where     := l_where || ' AND UPPER(user_name) LIKE ''%' || UPPER(p_user_name) || '%''';
---   END IF;
---
---   IF p_log_type IS NOT NULL THEN
---      l_where     := l_where || ' AND UPPER(log_type) LIKE ''%' || UPPER(p_log_type) || '%''';
---   END IF;
---
---   IF p_channel_type IS NOT NULL THEN
---      l_where     := l_where || ' AND UPPER(channel_type) LIKE ''%' || UPPER(p_channel_type) || '%''';
---   END IF;
+   IF p_user_name IS NOT NULL THEN
+      l_where     := l_where || ' AND UPPER(user_name) LIKE ''%' || UPPER(p_user_name) || '%''';
+   END IF;
+
+   IF p_log_type IS NOT NULL THEN
+      l_where     := l_where || ' AND UPPER(log_type) LIKE ''%' || UPPER(p_log_type) || '%''';
+   END IF;
+
+   IF p_channel_type IS NOT NULL THEN
+      l_where     := l_where || ' AND UPPER(channel_type) LIKE ''%' || UPPER(p_channel_type) || '%''';
+   END IF;
 
 
 
