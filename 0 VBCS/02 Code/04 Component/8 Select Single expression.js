a5fe141c0f175f490927c5c919a8392592eb6f60
Dinesh <pydinesh3@gmail.com>
2024-03-09 00:53:52 +0530

    Create 8 Select Single expression.js

---
+++ 0 VBCS/02 Code/04 Component/8 Select Single expression.js
@@ -0,0 +1,5 @@
+.context.fetchOptions.filterCriterion
+.context.fetchOptions.filterCriterion.text
+
+configuration.hookHandler.context.fetchOptions.filterCriterion
+configuration.hookHandler.context.fetchOptions.filterCriterion.text
